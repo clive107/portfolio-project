41f3b3988e1a8ec2ea9156f6a2b1e670c5a7f167
clive107 <clive107@gmail.com>
2018-09-13 16:49:29 +0800

    Jobs app up and running

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings # this import the settings file we have in the portfolio
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# whenever you're adding some static files, we want to have the media url & root
